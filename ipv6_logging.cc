404f5dbd2d0b009ed72d2e370dd082687f84f11b
Andrei Bykov <dronbykov@gmail.com>
2019-02-02 03:17:57 +0300

    cpp script has been completed

--- ipv6_logging.cc
+++ ipv6_logging.cc
@@ -3,7 +3,8 @@
 #include <iostream>
 #include <string>
 #include <sys/socket.h>
-#include<netinet/ip6.h>
+#include <linux/ipv6.h>
+#include <linux/kernel.h>
 
 #include "bcc/BPF.h"
 
@@ -30,7 +31,31 @@ int ipv6_filter(struct __sk_buff *skb) {
 		return 0;
 })";
 
-int main() {
+const int ETH_HLEN = 14;
+const int IPV6_ADDR_SHIFT = 8;
+const int IPV6_ADDR_LEN = 16;
+
+void printIPv6Address(unsigned char* buf, int start_address_byte) {
+    for (int i = start_address_byte; i < start_address_byte + IPV6_ADDR_LEN; i += 2) {
+        std::cout << (int)buf[i] * 256 + (int)buf[i + 1];
+        if (i + 2 < start_address_byte + IPV6_ADDR_LEN) {
+            std::cout << ":";
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+
+    std::cout << "USAGE: "<< argv[0] << " [-i <if_name>]" << std::endl;
+
+    std::string interface = "eth0";
+
+    if (argc == 3 && strcmp(argv[1], "-i") == 0) {
+        interface = argv[2];
+    }
+
+    std::cout << "interface = " << interface << std::endl;
+
     ebpf::BPF bpf;
     auto init_res = bpf.init(BPF_PROGRAM);
 
@@ -47,34 +72,27 @@ int main() {
         return 1;
     }
 
-    int sock = bpf_open_raw_sock("lo");
+    int sock = bpf_open_raw_sock(interface.c_str());
 
     if (bpf_attach_socket(sock, prog_fd) != 0){
         std::cerr << "Error creating socket" << std::endl;
         return 1;
     }
 
+    std::cout << "Socket attached" << std::endl;
+
     unsigned char *buf = new unsigned char[2048];
     long size;
     while (true) {
-        size = read(sock,buf, 2048);
+        size = read(sock, buf, 2048);
         if(size != -1) {
-            //  struct ip6_hdr *ip_packet = (struct ip6_hdr *) buf+14;
+            std::cout << std::hex << "Src host: ";
+            printIPv6Address(buf, ETH_HLEN + IPV6_ADDR_SHIFT);
 
-            struct in6_addr *src = (struct in6_addr *) &buf[22];
-            struct in6_addr *dst = (struct in6_addr *) &buf[38];
-            for(int i = 0; i < 8; i++)
-                std::cout << src->__in6_u.__u6_addr16[i] << ".";
-            std::cout << " - src " << std::endl;
+            std::cout << "     Dest host: ";
+            printIPv6Address(buf, ETH_HLEN + IPV6_ADDR_SHIFT + IPV6_ADDR_LEN);
 
-            for(int i = 0; i < 8; i++)
-                std::cout << dst->__in6_u.__u6_addr16[i] << ".";
-            std::cout << " - dst" << std::endl;
-            // for (int i = 0; i < size; i++) {
-            //     std::cout << i << " : " << (int)buf[i] <<std::endl;
-            // }
+            std::cout << std::endl;
         }
     }
-
-    std::cout << "Great!" << std::endl;
 }
